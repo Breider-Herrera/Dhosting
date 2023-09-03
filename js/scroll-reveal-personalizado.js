b355e928ebf31fa916b9180db3a480ca6bdcd044
Breider Herrera <herrera230397@gmail.com>
2023-09-03 11:42:10 -0500

    Dhosting 1.0

---
+++ js/scroll-reveal-personalizado.js
@@ -0,0 +1,84 @@
+window.sr = ScrollReveal();
+
+
+
+//Nav bar
+sr.reveal('.navbar', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '-100px'
+});
+
+
+
+//Hero section
+sr.reveal('.hero-img-bot', {
+	duration: 3000,
+	origin: 'bottom',
+	distance: '-70px'
+});
+sr.reveal('.welcome-to', {
+	duration: 4500,
+	origin: 'top',
+	distance: '-50px'
+});
+sr.reveal('.welcome-msj', {
+	duration: 4500,
+	origin: 'top',
+	distance: '-50px'
+});
+sr.reveal('.hero-lower-image', {
+	duration: 4500,
+	origin: 'top',
+	distance: '-70px'
+});
+
+
+
+//About Dhosting
+sr.reveal('.about-dhosting-title', {
+	duration: 1500,
+	origin: 'top',
+	distance: '-30px'
+});
+
+
+
+//SERVICES 
+sr.reveal('.domain', { 
+	duration: 4500,
+	origin: 'right',
+	distance: '-60px'
+});
+sr.reveal('.hosting', {
+	duration: 4500,
+	origin: 'top',
+	distance: '-70px'
+});
+sr.reveal('.design', {
+	duration: 4500,
+	origin: 'left',
+	distance: '-60px'
+});
+
+
+
+//Section Memberships
+sr.reveal('.title-memberships', { 
+	duration: 4500,
+	origin: 'bottom',
+	distance: '-60px'
+});
+sr.reveal('.description-section', {
+	duration: 9500,
+	origin: 'top'
+});
+
+
+
+//Footer
+sr.reveal('.footer-logo', {
+	duration: 5000,
+	origin: 'top',
+	distance: '-60px'
+});
